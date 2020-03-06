3416d737e37d400b58135f6b17502cbc28eb80cb
kimlherumali <kimlherumali@gmail.com>
2020-03-07 04:54:53 +0800

    Corrected error on designer.vb

--- Auto Encoding of Grades in SIS/My Project/Application.Designer.vb
+++ Auto Encoding of Grades in SIS/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Auto_Encoding_of_Grades_in_SIS.Form1
+            Me.MainForm = Global.Auto_Encoding_of_Grades_in_SIS.AEGS
         End Sub
     End Class
 End Namespace
